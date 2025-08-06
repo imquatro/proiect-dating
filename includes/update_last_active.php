0d581c9bc35a34305607d93b697689b9319f0154
imquatro <serverboost93@gmail.com>
2025-08-06 15:15:42 +0300

    Delete includes directory

--- includes/update_last_active.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_SESSION['user_id'])) {
-    try {
-        $stmt = $db->prepare('UPDATE users SET last_active = NOW() WHERE id = ?');
-        $stmt->execute([$_SESSION['user_id']]);
-    } catch (PDOException $e) {
-        // Ignorăm erorile de actualizare a activității
-    }
-}
