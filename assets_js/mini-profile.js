908fea6d820f1b0c8c1e36f6b5f8f02e9fd07843
imquatro <serverboost93@gmail.com>
2025-08-06 15:15:24 +0300

    Delete assets_js directory

--- assets_js/mini-profile.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const avatar = document.querySelector('.mini-profile-avatar');
-    if (avatar) {
-        avatar.addEventListener('click', function() {
-            console.log('Mini profile avatar clicked');
-        });
-    }
-});
