5d294c18efb74c98abe82fe7bb4535b2630737ed
imquatro <serverboost93@gmail.com>
2025-08-10 18:25:07 +0300

    Add files via upload

--- diverse.php
+++ diverse.php
@@ -5,7 +5,7 @@ ob_start();
 <div class="diverse-container">
     <div id="diversePanel" class="diverse-panel">
         <div class="diverse-grid">
-            <div class="diverse-item"><button type="button"><img src="img/admin.png" alt="Admin Panel"></button></div>
+            <div class="diverse-item"><button type="button" id="open-admin-panel"><img src="img/admin.png" alt="Admin Panel"></button></div>
             <div class="diverse-item"><button type="button"><img src="img/banca.png" alt="Banca"></button></div>
             <div class="diverse-item"><button type="button"><img src="img/lideri.png" alt="Lideri"></button></div>
             <div class="diverse-item"><button type="button"><img src="img/setari.png" alt="Setari"></button></div>
@@ -16,5 +16,7 @@ ob_start();
 $content = ob_get_clean();
 $pageTitle = 'Diverse';
 $pageCss = 'assets_css/diverse.css';
+$extraCss = ['farm_admin/admin-panel.css'];
+$extraJs = '<script src="farm_admin/admin-panel.js"></script>';
 include 'template.php';
 ?>
