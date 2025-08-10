fccc4a9a217631089abc67d87ede905ccb754441
imquatro <serverboost93@gmail.com>
2025-08-10 16:54:36 +0300

    Add files via upload

--- diverse.php
+++ diverse.php
@@ -4,17 +4,17 @@ ob_start();
 ?>
 <div class="diverse-container">
     <div id="diversePanel" class="diverse-panel">
-        <div class="diverse-grid">
-            <div class="diverse-item"><img src="img/admin.png" alt="Admin Panel"></div>
-            <div class="diverse-item"><img src="img/banca.png" alt="Banca"></div>
-            <div class="diverse-item"><img src="img/lideri.png" alt="Lideri"></div>
-            <div class="diverse-item"><img src="img/setari.png" alt="Setari"></div>
-        </div>
-    </div>
-</div>
-<?php
-$content = ob_get_clean();
-$pageTitle = 'Diverse';
-$pageCss = 'assets_css/diverse.css';
-include 'template.php';
+        <div class="diverse-grid">␊
+            <button type="button" class="diverse-button"><img src="img/admin.png" alt="Admin Panel"></button>
+            <button type="button" class="diverse-button"><img src="img/banca.png" alt="Banca"></button>
+            <button type="button" class="diverse-button"><img src="img/lideri.png" alt="Lideri"></button>
+            <button type="button" class="diverse-button"><img src="img/setari.png" alt="Setari"></button>
+        </div>␊
+    </div>␊
+</div>␊
+<?php␊
+$content = ob_get_clean();␊
+$pageTitle = 'Diverse';␊
+$pageCss = 'assets_css/diverse.css';␊
+include 'template.php';␊
 ?>
