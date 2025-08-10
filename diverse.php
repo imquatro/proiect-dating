8d036b5d30457a1c7ed3a3d67c3fa111865826eb
imquatro <serverboost93@gmail.com>
2025-08-10 16:43:24 +0300

    Add files via upload

--- diverse.php
+++ diverse.php
@@ -5,15 +5,10 @@ ob_start();
 <div class="diverse-container">
     <div id="diversePanel" class="diverse-panel">
         <div class="diverse-grid">
-            <div class="diverse-item"></div>
-            <div class="diverse-item"></div>
-            <div class="diverse-item"></div>
-            <div class="diverse-item"></div>
-            <div class="diverse-item"></div>
-            <div class="diverse-item"></div>
-            <div class="diverse-item"></div>
-            <div class="diverse-item"></div>
-            <div class="diverse-item"></div>
+            <div class="diverse-item"><img src="img/admin.png" alt="Admin Panel"></div>
+            <div class="diverse-item"><img src="img/banca.png" alt="Banca"></div>
+            <div class="diverse-item"><img src="img/lideri.png" alt="Lideri"></div>
+            <div class="diverse-item"><img src="img/setari.png" alt="Setari"></div>
         </div>
     </div>
 </div>
