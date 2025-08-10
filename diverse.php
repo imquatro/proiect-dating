49186c7d11ff7c981b524bf9af952f08f7f477e3
imquatro <serverboost93@gmail.com>
2025-08-10 17:23:53 +0300

    Add files via upload

--- diverse.php
+++ diverse.php
@@ -4,17 +4,17 @@ ob_start();
 ?>
 <div class="diverse-container">
     <div id="diversePanel" class="diverse-panel">
-        <div class="diverse-grid">␊
-            <button type="button" class="diverse-button"><img src="img/admin.png" alt="Admin Panel"></button>
-            <button type="button" class="diverse-button"><img src="img/banca.png" alt="Banca"></button>
-            <button type="button" class="diverse-button"><img src="img/lideri.png" alt="Lideri"></button>
-            <button type="button" class="diverse-button"><img src="img/setari.png" alt="Setari"></button>
-        </div>␊
-    </div>␊
-</div>␊
-<?php␊
-$content = ob_get_clean();␊
-$pageTitle = 'Diverse';␊
-$pageCss = 'assets_css/diverse.css';␊
-include 'template.php';␊
+        <div class="diverse-grid">
+            <div class="diverse-item"><button type="button"><img src="img/admin.png" alt="Admin Panel"></button></div>
+            <div class="diverse-item"><button type="button"><img src="img/banca.png" alt="Banca"></button></div>
+            <div class="diverse-item"><button type="button"><img src="img/lideri.png" alt="Lideri"></button></div>
+            <div class="diverse-item"><button type="button"><img src="img/setari.png" alt="Setari"></button></div>
+        </div>
+    </div>
+</div>
+<?php
+$content = ob_get_clean();
+$pageTitle = 'Diverse';
+$pageCss = 'assets_css/diverse.css';
+include 'template.php';
 ?>
