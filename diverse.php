5c4e51071f490a6b08c0c6d95715f36b5c153c97
imquatro <serverboost93@gmail.com>
2025-08-11 19:45:45 +0300

    Add files via upload

--- diverse.php
+++ diverse.php
@@ -3,7 +3,6 @@ $activePage = 'diverse';
 ob_start();
 ?>
 <div class="diverse-container">
-    <div id="diversePanel" class="diverse-panel">
         <div class="diverse-grid">
             <div class="diverse-item"><button type="button" id="open-admin-panel"><img src="img/admin.png" alt="Admin Panel"></button></div>
             <div class="diverse-item"><button type="button"><img src="img/banca.png" alt="Banca"></button></div>
